0761a3fcccdf48ac37ae75c35e22a506f6eb04f4
NicoMigna <nicole.mignami@supsi.ch>
2026-02-04 10:57:35 +0100

    add all

---
+++ giorno3/es1/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+			let cnv = createCanvas(windowWidth, windowHeight)
+			cnv.position(0, 0)
+			cnv.style('z-index', '-1')
+			face.resize(face.width / 7 * 1.1, face.height / 7 * 1.1)
+		}
+
+function draw() {
+
+}
+
+//function draw() {
+  //  text("Hello World!", mouseX, mouseY);
+  //  fill(198, 171, 207);
+  //  textSize(random (5, 32));
+  //  textAlign(CENTER, CENTER);
+  //  textFont('Courier New');
+//}
